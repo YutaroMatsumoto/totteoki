98b42a7cb6d497eee7b530ce5450f64e727a3776
松本悠太郎 <matsumotoyuutarou@matsumotoyuutarounoMacBook-Pro.local>
2020-08-30 22:06:51 +0900

    ヘッダー実装（ログインボタン含むがログイン機能はまだ未実装）

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,4 @@
+<nav class="navbar navbar-light" style="background-color:#e3f2fd;">
+    <a class="navbar-brand" href="#">Totteoki（仮）</a>
+    <a class="btn btn-outline-primary my-2 my-sm-0" href="#">ログイン</a>
+</nav>
